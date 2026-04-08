28be46dd43747abc274d46c66423618701cf7efc
artem <sutalkuar808typ@gmail.com>
2026-04-08 06:38:02 +0000

    cardian: cache cleaning

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.base import Base
-from app.models.lookups import ManualLookups, ParsedLookups
-from app.models.users import Users
-from app.models.verdicts import Verdicts
-from app.models.watchlists import Watchlist, PriceAlerts
