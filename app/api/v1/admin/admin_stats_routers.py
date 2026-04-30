fdd0dab815b49e7afc2fb96c554d8fa3a0948ae6
artem <sutalkuar808typ@gmail.com>
2026-04-30 15:11:09 +0000

    cardian: fix: adding request to route

--- app/api/v1/admin/admin_stats_routers.py
+++ app/api/v1/admin/admin_stats_routers.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.requests import Request
 
 from app.schemas.stats_schema import StatsResponseSchema
 from app.repo.admin_repo.admin_stats_repo import AdminStatsRepo
@@ -14,7 +15,7 @@ admin_stats_router = APIRouter(prefix="/v1/admin/lookups", tags=['Admin (Lookups
 
 @admin_stats_router.get("/stats", summary="Get all platform stats", response_model=StatsResponseSchema)
 @rate_limiter.limit("100/15 seconds")
-async def get_platform_stats(get_current_admin_users: Users = Depends(get_current_admin_user), session: AsyncSession = Depends(get_db)) -> StatsResponseSchema:
+async def get_platform_stats(request: Request, get_current_admin_users: Users = Depends(get_current_admin_user), session: AsyncSession = Depends(get_db)) -> StatsResponseSchema:
     stats: StatsResponseSchema | None = await AdminStatsRepo.get_platform_stats(session=session)
 
     if stats is None:
