8cf30390dac649a7a8dca4fe7c56966b554487bb
artem <sutalkuar808typ@gmail.com>
2026-05-02 07:45:49 +0000

    cardian: fix: bugs

--- app/background/lookups_processing_tasks.py
+++ app/background/lookups_processing_tasks.py
@@ -52,11 +52,15 @@ async def async_process_manual_lookup(self, lookup_id: int) -> None:
             car_info = CarSchema.model_validate(lookup)
         logger.info("lookup found")
         
-        predicted_price = predict_service.predict(data_to_predict=car_info)
+        try:
+            predicted_price = predict_service.predict(data_to_predict=car_info)
+            logger.info(f"price predicted: {predicted_price}")
+        
+        except:
+            logger.error(f"predicting error: {e}")
 
         info_to_analyze = GroqAnalyzeRequestSchema(**car_info.model_dump(), predicted_price=predicted_price)
 
-        logger.info(f"price predicted: {predicted_price}")
 
         try:
 
